06ba68bd4d8416f7eace4f280690cdcf874b4696
Rui Ying (UEA MAC) <r.ying@uea.ac.uk>
2025-08-23 22:42:21 +0100

    update test and docs after rm spk files

--- test/test_colormap.py
+++ test/test_colormap.py
@@ -7,16 +7,11 @@ def test_txt_palette():
     assert matplotlib.colors.to_hex(cmap.colors[0]) == '#feb483'
 
 
-def test_spk_palette():
-    cmap = CommunityPalette().get_palette('medium_rainbow')
-    assert matplotlib.colors.to_hex(cmap.colors[0]) == '#cc00ff'
-
-
 def test_xml_palette():
     cmap = CommunityPalette().get_palette('Section')
     assert matplotlib.colors.to_hex(cmap.colors[0]) == '#c2ccb8'
 
 
 def test_alt_init():
-    hex_codes = CommunityPalette('medium_rainbow').to_hex()
-    assert hex_codes[0] == '#cc00ff'
+    hex_codes = CommunityPalette('my_rainbow').to_hex()
+    assert hex_codes[0] == '#320064'
